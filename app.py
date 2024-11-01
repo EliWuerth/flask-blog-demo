26f1d43da97816d135bf6c9ff4c00d5499207681
Eli Wuerth <eliwuerth@Elis-MacBook-Pro.local>
2024-11-01 15:50:06 -0500

    added the create post function
    added create tab to base and added in a message alert

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config["SECRET_KEY"] = "mysecretkey"
 
 
 # Function to open a connection to the database.db file
@@ -43,4 +43,45 @@ def index():
     return render_template('index.html', posts=rows)
 
 # route to create a post
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        # get form data
+        title = request.form['title']
+        content = request.form['content']
+
+        # validate form data
+        if not title and not content:
+            flash('Title and Content are required!')
+            return redirect(url_for('create'))
+        elif not content:
+            flash('Content is required!')
+            return redirect(url_for('create'))
+        elif not title:
+            flash('Title is required!')
+            return redirect(url_for('create'))
+
+        # get connection to database
+        conn = get_db_connection()
+
+        # create a cursor object
+        cur = conn.cursor()
+
+        # execute SQL query to insert a new post into the posts table
+        cur.execute("INSERT INTO posts (title, content) VALUES (?,?)", (title, content))
+
+        # commit the changes to the database
+        conn.commit()
+
+        # close the cursor and connection
+        cur.close()
+        conn.close()
+
+        # redirect to the index page
+        return redirect(url_for('index'))
+
+    else:
+        # render the create.html template
+        return render_template('create.html')
+
 app.run()
